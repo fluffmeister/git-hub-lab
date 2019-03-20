52ef04e0a36af7c52519e2a3569939f9651b2cb2
Dominic Jaurigue <Domjaurigue@gmail.com>
2019-03-20 05:34:49 -0700

    Commit 9 - Created loops.js file

---
+++ loop-questions/app.js
@@ -0,0 +1,40 @@
+console.log('hello')
+// Create a loop that logs the numbers from 0-99, (ascending)
+// for (let i=0; i=<99;i++){
+//     console.log("The number is:  " +i);
+// }
+
+// Create a loop that logs the numbers from 99-0 (descending)
+// for (let i=99; i>=0;i--){
+//     console.log("The number is:  " +i);
+// }
+// Create a loop that logs the numbers from 298-1, counting down by 3 (the code in the loop should run 99 times)
+// for (let i=298; i>=1;i--){
+//     console.log("The number is:  " +i /3);
+// }
+// Create a loop that logs the EVEN numbers from 0-98 (ascending)
+// let i;
+// for(i=0;i<98;i++){
+//     if((i%2) ==0){
+//         console.log('this number is ' +i);
+//     }
+// }
+// Create a loop that logs the ODD numbers from 98-0 (descending)
+// let i;
+// for(i=98;i>0;i--){
+//     if((i%2) !==0){
+//         console.log('this number is ' +i);
+//     }
+// }
+// Create a loop that logs the numbers from 49-72 (ascending)
+// for(let i=49;i<=72;i++){
+//     console.log('this number is: '+i);
+// }
+// Create a loop that logs the numbers from 81-26 (descending)
+// for(let i=81;i>=26;i--){
+//     console.log('this number is: '+i);
+// }
+// Create a loop that logs the numbers from 1-91 that are multiples of 3 (ascending)
+for (let i=1; i<91;i++){
+    console.log((i*3)+1*2);
+}
